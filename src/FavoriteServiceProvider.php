a5b7a090e69f319024dad8ceb38eea265fa7cd74
Andrey Manzadey <andrey.manzadey@gmail.com>
2022-03-31 05:14:40 +0300

    moved migration

--- src/FavoriteServiceProvider.php
+++ src/FavoriteServiceProvider.php
@@ -14,7 +14,7 @@ class FavoriteServiceProvider extends ServiceProvider
     public function boot() : void
     {
         $this->publishes([
-            __DIR__ . '/../migrations/create_favorites_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_favorites_table.php'),
+            __DIR__ . '/../database/migrations/create_favorites_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_favorites_table.php'),
         ], 'favorite-migrations');
     }
 }
