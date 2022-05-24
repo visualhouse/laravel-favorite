e8f1136d26a6c5106d69800b461b0b2f4025d34b
Andrey Manzadey <andrey.manzadey@gmail.com>
2022-05-24 05:12:09 +0300

    Update favorite.php

--- config/favorite.php
+++ config/favorite.php
@@ -3,6 +3,11 @@
 declare(strict_types=1);
 
 return [
+
+    /*
+     * The fully qualified class name of the favorite model.
+     */
     'model' => \Manzadey\LaravelFavorite\Models\Favorite::class,
+
 ];
 
