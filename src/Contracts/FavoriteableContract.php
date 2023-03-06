be1d7b77d10865d00cd2c66afe7a3d8d53b69189
Nicolas Gominet <nicolas.gominet@visualhouse.co>
2023-03-06 11:32:04 -0700

    Remove docblock when types are used.

--- src/Contracts/FavoriteableContract.php
+++ src/Contracts/FavoriteableContract.php
@@ -13,58 +13,36 @@ interface FavoriteableContract
 {
     /**
      * MorphMany Relationship
-     *
-     * @return MorphMany
      */
-    public function favoriteable() : MorphMany;
+    public function favoriteable(): MorphMany;
 
     /**
      * Add the current model to favorites of the specified user
-     *
-     * @param FavoriteabilityContract $user
-     *
-     * @return Model
      */
-    public function addFavorite(FavoriteabilityContract $user) : Model;
+    public function addFavorite(FavoriteabilityContract $user): Model;
 
     /**
      * Check the current model whether it is a favorite of the specified user
-     *
-     * @param FavoriteabilityContract $user
-     *
-     * @return bool
      */
-    public function isFavorite(FavoriteabilityContract $user) : bool;
+    public function isFavorite(FavoriteabilityContract $user): bool;
 
     /**
      * Check the current model whether it is a favorite
-     *
-     * @return bool
      */
-    public function isFavorites() : bool;
+    public function isFavorites(): bool;
 
     /**
      * Delete the current module from the favorites of the specified user
-     *
-     * @param FavoriteabilityContract $user
-     *
-     * @return void
      */
-    public function removeFavorite(FavoriteabilityContract $user) : void;
+    public function removeFavorite(FavoriteabilityContract $user): void;
 
     /**
      * The switch of the favorites of the specified user
-     *
-     * @param FavoriteabilityContract $user
-     *
-     * @return bool
      */
-    public function toggleFavorite(FavoriteabilityContract $user) : bool;
+    public function toggleFavorite(FavoriteabilityContract $user): bool;
 
     /**
      * Return a collection with the Users who marked as favorite this model.
-     *
-     * @return Collection
      */
-    public function favoriteBy() : Collection;
+    public function favoriteBy(): Collection;
 }
